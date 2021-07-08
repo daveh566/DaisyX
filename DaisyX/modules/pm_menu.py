59cd94656439238428d4f0a9f2d4b96fcc03f650
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-07-08 21:17:05 +0300

    Update pm_menu.py

--- DaisyX/modules/pm_menu.py
+++ DaisyX/modules/pm_menu.py
@@ -81,19 +81,19 @@ async def get_start_func(message, strings, edit=False):
     buttons.add(
         InlineKeyboardButton(strings["btn_lang"], callback_data="lang_btn"),
         InlineKeyboardButton(
-            strings["btn_source"], url="https://github.com/TeamDaisyX/"
+            strings["btn_source"], url="https://t.me/aspirer2/"
         ),
     )
     buttons.add(
         InlineKeyboardButton(strings["btn_channel"], url="https://t.me/DaisyXUpdates"),
         InlineKeyboardButton(
-            strings["btn_group"], url="https://t.me/DaisySupport_Official"
+            strings["btn_group"], url="https://t.me/intimacyfolkz"
         ),
     )
     buttons.add(
         InlineKeyboardButton(
             "👸🏼 Add Miss Nelly to your group",
-            url=f"https://telegram.me/daisyxbot?startgroup=true",
+            url=f"https://telegram.me/missnellyXbot?startgroup=true",
         )
     )
     # Handle error when user click the button 2 or more times simultaneously
